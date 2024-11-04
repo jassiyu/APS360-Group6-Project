98c909d2f71058113ec672e10961e1ac91974cdc
AndrewOkiya <60572042+AndrewOkiya@users.noreply.github.com>
2024-11-04 04:58:49 -0500

    Add files via upload

--- BaselineModel_ver11.3.2.py
+++ BaselineModel_ver11.3.2.py
@@ -5,10 +5,61 @@ import torch.optim as optim
 import random
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 # Data Loaders
-from torch.utils.data import Dataset,DataLoader
+from torch.utils.data import Dataset,DataLoader,Subset
 from torchvision import datasets, transforms
 from PIL import Image
+
+
+
+# Label mapping for GTSRB dataset
+label_mapping = {
+    0: "Speed limit (20km/h)",
+    1: "Speed limit (30km/h)",
+    2: "Speed limit (50km/h)",
+    3: "Speed limit (60km/h)",
+    4: "Speed limit (70km/h)",
+    5: "Speed limit (80km/h)",
+    6: "End of speed limit (80km/h)",
+    7: "Speed limit (100km/h)",
+    8: "Speed limit (120km/h)",
+    9: "No passing",
+    10: "No passing for vehicles over 3.5 metric tons",
+    11: "Right-of-way at the next intersection",
+    12: "Priority road",
+    13: "Yield",
+    14: "Stop",
+    15: "No vehicles",
+    16: "Vehicles over 3.5 metric tons prohibited",
+    17: "No entry",
+    18: "General caution",
+    19: "Dangerous curve to the left",
+    20: "Dangerous curve to the right",
+    21: "Double curve",
+    22: "Bumpy road",
+    23: "Slippery road",
+    24: "Road narrows on the right",
+    25: "Road work",
+    26: "Traffic signals",
+    27: "Pedestrians",
+    28: "Children crossing",
+    29: "Bicycles crossing",
+    30: "Beware of ice/snow",
+    31: "Wild animals crossing",
+    32: "End of all speed and passing limits",
+    33: "Turn right ahead",
+    34: "Turn left ahead",
+    35: "Ahead only",
+    36: "Go straight or right",
+    37: "Go straight or left",
+    38: "Keep right",
+    39: "Keep left",
+    40: "Roundabout mandatory",
+    41: "End of no passing",
+    42: "End of no passing by vehicles over 3.5 metric tons"
+}
+
 
 #################### import train data and validation ###################
 
@@ -87,10 +138,10 @@ def train(model, train_loader, val_loader, num_epochs=10, learning_rate=0.001):
         train_losses.append(avg_train_loss)  # Save train loss for this epoch
 
         # --- Validation Phase ---
+        torch.save(model.state_dict(), data_dir +"model_lr{0}_epoch{1}.pth".format(learning_rate,epoch))
         model.eval()
         val_loss = 0.0
         correct_val = 0
-
         with torch.no_grad():
             for imgs, labels in val_loader:
                 #############################################
@@ -113,7 +164,6 @@ def train(model, train_loader, val_loader, num_epochs=10, learning_rate=0.001):
         avg_val_loss = val_loss / len(val_loader.dataset)
         val_accuracy = 100 * correct_val / len(val_loader.dataset)
         val_losses.append(avg_val_loss)  # Save validation loss for this epoch
-
         # Print epoch summary
         print(f"Epoch [{epoch+1}/{num_epochs}], "
               f"Train Loss: {avg_train_loss:.4f}, Train Acc: {train_accuracy:.2f}%, "
@@ -169,19 +219,33 @@ def show_misclassified_images(images, true_labels, predicted_labels, num_images=
         plt.axis('off')
     plt.show()
 
-class TestDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None):
-        self.img_labels = pd.read_csv(annotations_file)  
-        self.img_dir = img_dir                          
-        self.transform = transform                      
+class GTSRBTestDataset(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None, use_label_names=False, label_mapping=None):
+        self.data = pd.read_csv(csv_file)  # Load CSV file
+        self.root_dir = root_dir  # Root directory where images are stored
+        self.transform = transform
+        self.use_label_names = use_label_names  # Flag to use label names
+        self.label_mapping = label_mapping  # Label mapping dictionary
 
     def __len__(self):
-        return len(self.img_labels)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])  
-        image = Image.open(img_name)                                         
-        label = self.img_labels.iloc[idx, 1]                                 
+        # Get image path
+        img_name = os.path.join(self.root_dir, self.data.iloc[idx, 7].strip())
+
+        #print(f"Loading image: {img_name}")
+
+        image = Image.open(img_name)
+
+        # Get the numerical label
+        label = int(self.data.iloc[idx, 6])
+
+        # If requested, use the label name instead of the numerical label
+        if self.use_label_names and self.label_mapping:
+            label = self.label_mapping.get(label, "Unknown")
+
+        # Apply transformations if provided
         if self.transform:
             image = self.transform(image)
 
@@ -208,7 +272,7 @@ if __name__ == '__main__':
   ])
 
   # Set the path to the dataset
-  data_dir = 'D:\\Study\\UT\\APS-360\\Project'
+  data_dir = 'D:\\Study\\UT\\APS-360\\Project\\'
 
   # Load the training dataset
   train_dataset = datasets.ImageFolder(data_dir + '/truncated_train', transform=train_transform)
@@ -225,16 +289,17 @@ if __name__ == '__main__':
   train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
   val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
   use_cuda = True
-  model = LeNet5(num_classes=43) # GTSRB has 43 classes
+  Newmodel = LeNet5(num_classes=43) # GTSRB has 43 classes
 
   if use_cuda and torch.cuda.is_available():
-    model.cuda()
+    Newmodel.cuda()
     print('CUDA is available!  Training on GPU ...')
   else:
     print('CUDA is not available.  Training on CPU ...')
-      
-  train_losses, val_losses = train(model, train_loader, val_loader,num_epochs=40,learning_rate=0.001)
 
+  train_losses, val_losses = train(Newmodel, train_loader, val_loader,num_epochs=40,learning_rate=0.001)
+
+  print('Training completed!')
   # Plot the loss curves
   plt.figure(figsize=(10, 5))
   plt.plot(train_losses, label='Training Loss')
@@ -244,7 +309,18 @@ if __name__ == '__main__':
   plt.title('Training and Validation Loss')
   plt.legend()
   plt.show()
-  img_dir = 'Test'
-  test_dataset = TestDataset(annotations_file='Test.csv', img_dir=img_dir, transform=val_transform)
-  test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
-  evaluate(model,test_loader)
+  """
+
+  #Test
+  test_dataset = GTSRBTestDataset(csv_file=data_dir +'Test.csv', root_dir=data_dir,transform=val_transform, label_mapping=label_mapping)
+  print('DatasetCreated.')
+  num_samples = 500
+  indices = list(range(len(test_dataset)))
+  random_sampled_indices = random.sample(indices, num_samples)
+  test_subset = Subset(test_dataset, random_sampled_indices)
+  # Create DataLoader for the subset
+  test_loader = DataLoader(dataset=test_subset, batch_size=32, shuffle=True)
+  print(f'Number of test samples: {len(test_subset)}')
+  Newmodel.load_state_dict(torch.load(data_dir +'model_lr0.001_epoch39.pth',weights_only=True))
+  evaluate(Newmodel,test_loader)
+  """
